29466f486ca3c3a7412d57608ef16df9969e7558
maximu211 <63352037+maximu211@users.noreply.github.com>
2024-03-28 11:07:12 +0200

    Update Program.cs

--- lab11/Program.cs
+++ lab11/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Threading.Tasks;
 
 class Program
@@ -6,8 +6,8 @@ class Program
     static void Main(string[] args)
     {
         Parallel.Invoke(
-            async () => await ProcessFirstPage(),
-            async () => await ProcessSecondPage()
+            async () => await GetData("https://www.thomann.de/gb/heavy_guitars.html"),
+            async () => await GetData("https://www.thomann.de/gb/t_models.html")
         );
 
         Console.WriteLine("Обидві сторінки товару було оброблено.");
@@ -15,10 +15,10 @@ class Program
         Console.ReadLine();
     }
 
-    static async Task ProcessFirstPage()
+    static async Task GetData(string url)
     {
         Console.WriteLine("Отримання даних з першої сторінки товару...");
-        string apiUrl = "https://www.thomann.de/gb/heavy_guitars.html";
+        string apiUrl = url;
 
         using (HttpClient client = new HttpClient())
         {
@@ -28,27 +28,6 @@ class Program
                 response.EnsureSuccessStatusCode();
                 string responseBody = await response.Content.ReadAsStringAsync();
                 Console.WriteLine($"Дані з першої сторінки товару: {responseBody}");
-            }
-            catch (HttpRequestException ex)
-            {
-                Console.WriteLine($"Помилка при виконанні запиту: {ex.Message}");
-            }
-        }
-    }
-
-    static async Task ProcessSecondPage()
-    {
-        Console.WriteLine("Отримання даних з другої сторінки товару...");
-        string apiUrl = "https://www.thomann.de/gb/headless_guitars.html";
-
-        using (HttpClient client = new HttpClient())
-        {
-            try
-            {
-                HttpResponseMessage response = await client.GetAsync(apiUrl);
-                response.EnsureSuccessStatusCode();
-                string responseBody = await response.Content.ReadAsStringAsync();
-                Console.WriteLine($"Дані з другої сторінки товару: {responseBody}");
             }
             catch (HttpRequestException ex)
             {
