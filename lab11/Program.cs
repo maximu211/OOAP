cd68deeebdd4b97df0ade58af5db587a35faa914
maximu211 <maxshtokaloo@gmail.com>
2024-04-02 22:11:55 +0300

    updated lab11

--- lab11/Program.cs
+++ lab11/Program.cs
@@ -1,59 +1,83 @@
 ﻿using System;
+using System.Net;
 using System.Threading.Tasks;
+using System.Xml;
+using HtmlAgilityPack;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Parallel.Invoke(
-            async () => await ProcessFirstPage(),
-            async () => await ProcessSecondPage()
+        Task<Guitar> task1 = GetData(
+            "https://www.thomann.de/gb/epiphone_flying_v_korina_aged_natural.htm"
         );
+        Task<Guitar> task2 = GetData("https://www.thomann.de/gb/schecter_c_1_sls_elite_fr_afb.htm");
 
-        Console.WriteLine("Обидві сторінки товару було оброблено.");
+        Parallel.Invoke(() => task1.Wait(), () => task2.Wait());
+
+        Guitar guitar1 = task1.Result;
+        Guitar guitar2 = task2.Result;
+
+        Console.WriteLine(guitar1.ToString());
+        Console.WriteLine();
+        Console.WriteLine(guitar2.ToString());
 
         Console.ReadLine();
     }
 
-    static async Task ProcessFirstPage()
+    static async Task<Guitar> GetData(string url)
     {
-        Console.WriteLine("Отримання даних з першої сторінки товару...");
-        string apiUrl = "https://www.thomann.de/gb/heavy_guitars.html";
+        Guitar guitar = new Guitar();
 
         using (HttpClient client = new HttpClient())
         {
             try
             {
-                HttpResponseMessage response = await client.GetAsync(apiUrl);
+                HttpResponseMessage response = await client.GetAsync(url);
                 response.EnsureSuccessStatusCode();
                 string responseBody = await response.Content.ReadAsStringAsync();
-                Console.WriteLine($"Дані з першої сторінки товару: {responseBody}");
+
+                HtmlDocument doc = new HtmlDocument();
+                doc.LoadHtml(responseBody);
+
+                HtmlNode h1Node = doc.DocumentNode.SelectSingleNode(
+                    "//h1[@class='fx-product-headline product-title__title' and @itemprop='name']"
+                );
+                guitar.Name = h1Node.InnerText.Trim();
+
+                HtmlNode price = doc.DocumentNode.SelectSingleNode("//div[@class='price']");
+                guitar.Price = price.InnerText.Trim();
+
+                List<string> descriptions = new List<string>();
+                HtmlNodeCollection descriptionNodes = doc.DocumentNode.SelectNodes(
+                    "//span[@class='fx-text fx-text--plus list-item__text']"
+                );
+
+                foreach (HtmlNode node in descriptionNodes)
+                {
+                    descriptions.Add(WebUtility.HtmlDecode(node.InnerText).Trim());
+                }
+
+                guitar.Description = descriptions;
+
+                return guitar;
             }
             catch (HttpRequestException ex)
             {
-                Console.WriteLine($"Помилка при виконанні запиту: {ex.Message}");
+                throw new Exception("Error: " + ex.Message);
             }
         }
     }
+}
 
-    static async Task ProcessSecondPage()
+class Guitar
+{
+    public string? Name { get; set; }
+    public string? Price { get; set; }
+    public List<string> Description { get; set; }
+
+    public override string ToString()
     {
-        Console.WriteLine("Отримання даних з другої сторінки товару...");
-        string apiUrl = "https://www.thomann.de/gb/headless_guitars.html";
-
-        using (HttpClient client = new HttpClient())
-        {
-            try
-            {
-                HttpResponseMessage response = await client.GetAsync(apiUrl);
-                response.EnsureSuccessStatusCode();
-                string responseBody = await response.Content.ReadAsStringAsync();
-                Console.WriteLine($"Дані з другої сторінки товару: {responseBody}");
-            }
-            catch (HttpRequestException ex)
-            {
-                Console.WriteLine($"Помилка при виконанні запиту: {ex.Message}");
-            }
-        }
+        return $"Назва: {Name}\nЦіна: {Price}\nОпис:\n{string.Join("\n", Description)}";
     }
 }
